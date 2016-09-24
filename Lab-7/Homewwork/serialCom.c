fd1b4058c86e954dab53472f6262c1af37d4a5ec
Ayush Agrawal <ayushagrawal@users.noreply.github.com>
2016-09-24 15:23:44 +0530

    Timer Initialization Function Added

---
+++ Lab-7/Homewwork/serialCom.c
@@ -0,0 +1,35 @@
+// Baud Rate = 1200
+// Timer 1 Reload Value = E6H
+
+////////// IMPORTING THE REQUIRED LIBRARY \\\\\\\\\\\\
+
+#include "AT89c5131.h"
+
+
+////////// Defining the Function for timer initializing \\\\\\\\\\
+
+void timer1_init()
+{
+	// Required Conditions:
+	// 1. Mode 2 - Auto Reload Mode
+	// 2. TH1 = E6H for baud rate of 1200 Hz
+	// 3. No Interrupt
+	// 4. Gate disabled and Timer Mode on
+	// 5. Enable the timer
+	
+	// TMOD = 0010 xxxx (1. & 4.)
+	TMOD = TMOD | 20H;		// Making only the desired bits to be '1'
+	TMOD = TMOD & 2FH;		// Making only the desired bits to be '0'
+	
+	// Setting up the baud rate (2.)
+	TH1  = 0E6H;
+	TL1  = 0E6H;
+	
+	// Disabling the Interrupt (3.)
+	// IE = xxxx 0xxx
+	IE   =  IE & 0F7H;
+	
+	// Enabling the Timer1
+	// TCON = 1xxx xxxx
+	TCON = TCON | 80H;
+}
